c7b048d599575bf506c7cb5cf9c088e79308c504
Sam Aaron <samaaron@gmail.com>
2009-11-11 01:05:25 +0100

    update to use new _extensions facility

---
+++ _extensions/filters.rb
@@ -0,0 +1,22 @@
+module Jekyll
+
+  module Filters
+    def date_to_day_ordinal(date)
+      case date.day
+      when 1, 21, 31 then "st"
+      when 2, 22     then "nd"
+      when 3, 23     then "rd"
+      else "th"
+      end
+    end
+
+    def date_to_single_digit_day(date)
+      date.day.to_i.to_s
+    end
+
+    def date_to_english_html_string(date)
+      date.strftime("%A the #{date_to_single_digit_day(date)}<sup>#{date_to_day_ordinal(date)}</sup> of %B, %Y")
+    end
+
+  end
+end
